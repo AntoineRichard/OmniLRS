e66a42d4eeb67ca7cb46983a5876805effb1e6af
AntoineRichard <antoine.richard@uni.lu>
2023-09-28 14:12:16 +0200

    reworked the data-acquisition pipeline and tested it with success

---
+++ src/labeling/auto_label.py
@@ -0,0 +1,150 @@
+from typing import List, Tuple, Dict, Union
+import omni.kit.actions.core
+import random, string
+from pxr import Usd
+import omni
+import os
+
+# import omni.replicator.core as rep
+from src.labeling.rep_utils import writerFactory
+import omni.replicator.core as rep
+
+#SYNTHETIC_WRITER = {"rgb": writeRgbData, 
+#                    "semantic_segmentation": writeSemanticData,
+#                    "instance_segmentation": writeInstanceSegmentationData}
+
+DATA_HASH = ''.join(random.sample(string.ascii_letters + string.digits, 16))
+
+class AutonomousLabeling:
+    """
+    AutonomousLabeling class. It is used to generate synthetic data from a USD scene."""
+
+    def __init__(self, prim_path: str,
+                       camera_name: str = "camera_annotations",
+                       camera_resolution: Tuple[int,int] = (640, 480),
+                       data_dir: str = "data",
+                       annotator_list: List[str] = ["rgb", "instance_segmentation", "semantic_segmentation"],
+                       image_format: str = "png",
+                       annot_format: str = "json",
+                       element_per_folder: int = 1000,
+                       add_noise_to_rgb: bool = False,
+                       sigma: float = 5.0,
+                       seed: int = 42,
+                       ) -> None:
+        """
+        Initialize the AutonomousLabeling class.
+
+        Args:
+            prim_path (str): The path of the USD scene.
+            camera_name (str, optional): The name of the camera. Defaults to "camera".
+            camera_resolution (Tuple[int,int], optional): The resolution of the camera. Defaults to (640, 480).
+            data_dir (str, optional): The directory where the synthetic data will be stored. Defaults to ".".
+            annotator_list (List[str], optional): The list of annotators that will be used to generate the synthetic data.
+                                                  Defaults to ["rgb, instance_segmentation, semantic_segmentation"].
+            image_format (str, optional): The format of the images. Defaults to "png".
+            annot_format (str, optional): The format of the annotations. Defaults to "json".
+            element_per_folder (int, optional): The number of elements per folder. Defaults to 10000.
+            add_noise_to_rgb (bool, optional): Whether to add noise to the RGB data. Defaults to False.
+            sigma (float, optional): The standard deviation of the noise. Defaults to 5.0.
+            seed (int, optional): The seed used to generate the random numbers. Defaults to 42."""
+
+        # Camera parameters
+        self.camera_name = camera_name
+        self.camera_resolution = camera_resolution
+
+        # Data storage parameters
+        self.data_hash = ''.join(random.sample(string.ascii_letters + string.digits, 16))
+        self.data_dir = os.path.join(data_dir, self.data_hash)
+
+        # Synthetic data parameters
+        self.annotator_list = annotator_list
+        self.add_noise_to_rgb = add_noise_to_rgb
+        self.sigma = sigma
+        self.seed = seed
+        self.image_format = image_format
+        self.annot_format = annot_format
+        self.element_per_folder = element_per_folder
+        cfg = self.formatWriterConfig()
+        self.synthetic_writers = {name: writerFactory(name, **cfg) for name in annotator_list}
+
+        self.stage = omni.usd.get_context().get_stage()
+        self.meta_prim = self.stage.GetPrimAtPath(prim_path)
+        self.annotator = {}
+        self.synth_counter = 0
+
+    def formatWriterConfig(self) -> Dict[str, Union[float, bool, int, str]]:
+        cfg = {}
+        cfg["root_path"] = self.data_dir
+        cfg["element_per_folder"] = self.element_per_folder
+        cfg["image_format"] = self.image_format
+        cfg["annot_format"] = self.annot_format
+        cfg["add_noise"] = self.add_noise_to_rgb
+        cfg["sigma"] = self.sigma
+        cfg["seed"] = self.seed
+        return cfg
+
+    def findCameraForAnnotation(self, camera_name: str) -> None:
+        """
+        Find the camera prim and path of the camera that will be used to generate the synthetic data.
+        
+        Args:
+            camera_name (str): The name of the camera."""
+        
+        for prim in Usd.PrimRange(self.meta_prim):
+            if prim.GetName() == camera_name:
+                self.camera_prim = prim
+                self.camera_path = str(prim.GetPath())
+
+    def load(self) -> None:
+        """
+        Finds the camera and enables the collection of RGB, instance segmentation, and semantic segmentation."""
+
+        self.findCameraForAnnotation(self.camera_name)
+        self.setCamera(self.camera_path, self.camera_resolution)
+        self.enableRGBData()
+        self.enableInstanceData() 
+        self.enableSemanticData()
+    
+    def setCamera(self, camera_path: str, res=(640, 480)) -> None:
+        """
+        Set the camera resolution that will be used to generate the synthetic data.
+        
+        Args:
+            camera_path (str): The path to the camera.
+            res (tuple, optional): The resolution of the camera. Defaults to (640, 480)."""
+        
+        self.render_product = rep.create.render_product(camera_path, res)
+
+    def enableRGBData(self) -> None:
+        """
+        Enable the collection of RGB data."""
+
+        rgb_annot = rep.AnnotatorRegistry.get_annotator("rgb")
+        self.annotator["rgb"] = rgb_annot
+        rgb_annot.attach([self.render_product])
+
+    def enableSemanticData(self) -> None:
+        """
+        Enable the collection of semantic segmentation data."""
+
+        semantic_annot = rep.AnnotatorRegistry.get_annotator("semantic_segmentation", 
+                                                            init_params={'colorize':True})
+        self.annotator["semantic_segmentation"] = semantic_annot
+        semantic_annot.attach([self.render_product])
+
+    def enableInstanceData(self) -> None:
+        """
+        Enable the collection of instance segmentation data."""
+
+        instance_annotator = rep.AnnotatorRegistry.get_annotator("instance_segmentation", 
+                                                                init_params={'colorize':True})
+        self.annotator["instance_segmentation"] = instance_annotator
+        instance_annotator.attach([self.render_product])
+
+    def record(self) -> None:
+        """
+        Record a frame of synthetic data."""
+
+        for name, annotator in self.annotator.items():
+            self.synthetic_writers[name].write(annotator.get_data())
+        self.synth_counter += 1
---
+++ src/labeling/rep_utils.py
@@ -0,0 +1,254 @@
+from typing import Any, Union, Dict
+import numpy as np
+import json 
+import cv2
+import os
+
+
+class BaseWriter:
+    def __init__(self, root_path:str,
+                       name: str,
+                       element_per_folder: int = 10000,
+                       **kwargs) -> None:
+        self.root_path = root_path
+        self.data_path = os.path.join(root_path, name)
+        self.element_per_folder = element_per_folder
+        self.counter = 0
+        self.image_fix_name_size = len(str(element_per_folder))
+        self.makeFolder()
+
+    def makeFolder(self) -> None:
+        """
+        Make a folder to store the data."""
+
+        if self.counter % self.element_per_folder == 0:
+            self.current_folder = os.path.join(self.data_path, str(self.counter // self.element_per_folder))
+            os.makedirs(self.current_folder, exist_ok=True)
+            self.counter = 0
+
+    def write(self, data: np.ndarray, **kwargs) -> None:
+        """
+        Write the data to a file.
+        
+        Args:
+            data (np.ndarray): The data to write.
+            **kwargs: Additional arguments."""
+
+        raise NotImplementedError
+
+
+class WriteRGBData(BaseWriter):
+    """
+    Write RGB data to a file."""
+
+    def __init__(self, root_path: str,
+                       name: str = "rgb",
+                       element_per_folder: int = 10000,
+                       image_format: str = "png",
+                       add_noise: bool = False,
+                       sigma: float = 5.0,
+                       seed: int = 42,
+                       **kwargs) -> None:
+        """
+        Initialize the WriteRGBData class.
+        
+        Args:
+            root_path (str): The root path of the data.
+            name (str, optional): The name of the data. Defaults to "rgb".
+            element_per_folder (int, optional): The number of elements per folder. Defaults to 10000.
+            image_format (str, optional): The image format. Defaults to "png".
+            add_noise (bool, optional): Whether to add noise to the RGB data. Defaults to False.
+            sigma (float, optional): The standard deviation of the Gaussian noise. Defaults to 5.0.
+            seed (int, optional): The seed used to generate the random numbers. Defaults to 42."""
+        
+        super().__init__(root_path, name, element_per_folder)
+        self.image_format = image_format
+        self.add_noise = add_noise
+        self.sigma = sigma
+        self.rng = np.random.default_rng(seed)
+
+    def imageGaussianNoise(self, data_in: np.ndarray) -> np.ndarray:
+        """
+        Add Gaussian noise to the RGB data.
+        
+        Args:
+            data_in (np.ndarray): The RGB data.
+            
+        Returns:
+            np.ndarray: The RGB data with Gaussian noise."""
+        
+        return (data_in + self.rng.normal(loc=0.0,scale=self.sigma,size=data_in.shape)).astype(np.uint8)
+
+    def write(self, data: np.ndarray, **kwargs) -> None:
+        """
+        Write RGB data to a file.
+        
+        Args:
+            data (np.ndarray): The RGB data.
+            **kwargs: Additional arguments."""
+        
+        self.makeFolder()
+        rgb_image = np.frombuffer(data, dtype=np.uint8).reshape(*data.shape, -1)
+        rgb_image = np.squeeze(rgb_image)
+        rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGBA2BGRA)
+        if self.add_noise:
+            rgb_image = self.imageGaussianNoise(rgb_image)
+        cv2.imwrite(os.path.join(self.current_folder, f'{self.counter:0{self.image_fix_name_size}d}.'+self.image_format), rgb_image)
+        self.counter += 1
+
+
+class WriteSemanticData(BaseWriter):
+    """
+    Write semantic segmentation data to a file."""
+
+    def __init__(self, root_path: str,
+                       name: str = "semantic_segmentation",
+                       element_per_folder: int = 10000,
+                       image_format: str = "png",
+                       annot_format: str = "json",
+                       **kwargs) -> None:
+        """
+        Initialize the WriteSemanticData class.
+        
+        Args:
+            root_path (str): The root path of the data.
+            name (str, optional): The name of the data. Defaults to "semantic_segmentation".
+            element_per_folder (int, optional): The number of elements per folder. Defaults to 10000.
+            image_format (str, optional): The image format. Defaults to "png".
+            annot_format (str, optional): The annotation format. Defaults to "json".
+            **kwargs: Additional arguments."""
+        
+        super().__init__(root_path, name, element_per_folder)
+        self.image_format = image_format
+        self.annot_format = annot_format
+
+    def makeFolder(self) -> None:
+        """
+        Make a folder to store the data."""
+
+        if self.counter % self.element_per_folder == 0:
+            self.current_folder_image = os.path.join(self.data_path, str(self.counter // self.element_per_folder))
+            self.current_folder_labels = os.path.join(self.data_path + "_id_label", str(self.counter // self.element_per_folder))
+            os.makedirs(self.current_folder_image, exist_ok=True)
+            os.makedirs(self.current_folder_labels, exist_ok=True)
+            self.counter = 0
+
+    def write(self, data: Dict[str, np.ndarray], **kwargs) -> None:
+        """
+        Write semantic segmentation data to a file.
+
+        Args:
+            data (Dict): The semantic segmentation data.
+            **kwargs: Additional arguments."""
+
+        self.makeFolder()
+        id_to_labels = data['info']['idToLabels']
+        sem_image_data = np.frombuffer(data['data'], dtype=np.uint8).reshape(*data["data"].shape, -1)
+        sem_image_data = np.squeeze(sem_image_data)
+        sem_image_data = cv2.cvtColor(sem_image_data, cv2.COLOR_RGBA2BGRA)
+        with open(os.path.join(self.current_folder_labels, f'{self.counter:0{self.image_fix_name_size}d}.'+self.annot_format), 'w') as f:
+            json.dump(id_to_labels, f)
+        cv2.imwrite(os.path.join(self.current_folder_image, f'{self.counter:0{self.image_fix_name_size}d}.'+self.image_format), sem_image_data)
+        self.counter += 1
+
+
+class WriteInstanceData(BaseWriter):
+    """
+    Write instance semantic segmentation data to a file."""
+    
+    def __init__(self, root_path: str,
+                       name: str = "semantic_segmentation",
+                       element_per_folder: int = 10000,
+                       image_format: str = "png",
+                       annot_format: str = "json",
+                       **kwargs) -> None:
+        """
+        Initialize the WriteInstanceData class.
+        
+        Args:
+            root_path (str): The root path of the data.
+            name (str, optional): The name of the data. Defaults to "semantic_segmentation".
+            element_per_folder (int, optional): The number of elements per folder. Defaults to 10000.
+            image_format (str, optional): The image format. Defaults to "png".
+            annot_format (str, optional): The annotation format. Defaults to "json".
+            **kwargs: Additional arguments."""
+        
+        super().__init__(root_path, name, element_per_folder)
+        self.image_format = image_format
+        self.annot_format = annot_format
+
+    def makeFolder(self) -> None:
+        """
+        Make a folder to store the data."""
+
+        if self.counter % self.element_per_folder == 0:
+            self.current_folder_image = os.path.join(self.data_path, str(self.counter // self.element_per_folder))
+            self.current_folder_labels = os.path.join(self.data_path + "_id_label", str(self.counter // self.element_per_folder))
+            self.current_folder_semantics = os.path.join(self.data_path + "_id_semantic", str(self.counter // self.element_per_folder))
+            os.makedirs(self.current_folder_image, exist_ok=True)
+            os.makedirs(self.current_folder_labels, exist_ok=True)
+            os.makedirs(self.current_folder_semantics, exist_ok=True)
+            self.counter = 0
+
+    def write(self, data: Dict[str, np.ndarray], **kwargs) -> None:
+        """
+        Write instance segmentation data to a file.
+
+        Args:
+            data (Dict): The instance segmentation data.
+            **kwargs: Additional arguments."""
+
+        self.makeFolder()
+        id_to_labels = data['info']['idToLabels']
+        id_to_semantic = data['info']['idToSemantics']
+        instance_image_data = np.frombuffer(data['data'], dtype=np.uint8).reshape(*data["data"].shape, -1)
+        instance_image_data = np.squeeze(instance_image_data)
+        instance_image_data = cv2.cvtColor(instance_image_data, cv2.COLOR_RGBA2BGRA)
+        with open(os.path.join(self.current_folder_labels, f'{self.counter:0{self.image_fix_name_size}d}.'+self.annot_format), 'w') as f:
+            json.dump(id_to_labels, f)
+        with open(os.path.join(self.current_folder_semantics, f'{self.counter:0{self.image_fix_name_size}d}.'+self.annot_format), 'w') as f:
+            json.dump(id_to_semantic, f)
+        cv2.imwrite(os.path.join(self.current_folder_image, f'{self.counter:0{self.image_fix_name_size}d}.'+self.image_format), instance_image_data)
+        self.counter += 1
+
+
+class WriterFactory:
+    """
+    A factory class to create writers."""
+
+    def __init__(self):
+        self.writers = {}
+
+    def registerWriter(self, name: str, writer: BaseWriter, **kwargs):
+        """
+        Register a writer to the factory.
+        
+        Args:
+            name (str): The name of the writer.
+            writer (BaseWriter): The writer to register.
+            **kwargs: Additional arguments."""
+        
+        self.writers[name] = writer
+
+    def __call__(self, name: str, **kwargs) -> BaseWriter:
+        """
+        Create a writer.
+        
+        Args:
+            name (str): The name of the writer.
+            **kwargs: Additional arguments.
+            
+        Returns:
+            BaseWriter: The writer.
+        
+        Raises:
+            AssertionError: If the writer is not registered."""
+        
+        assert name in self.writers, f"Writer {name} not registered"
+
+        return self.writers[name](name=name,**kwargs)
+    
+writerFactory = WriterFactory()
+writerFactory.registerWriter("rgb", WriteRGBData)
+writerFactory.registerWriter("semantic_segmentation", WriteSemanticData)
+writerFactory.registerWriter("instance_segmentation", WriteInstanceData)
