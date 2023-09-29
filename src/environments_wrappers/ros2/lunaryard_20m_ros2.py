f4a877ef420ef3c937f939e663ef60c7dcd86763
AntoineRichard <antoine.richard@uni.lu>
2023-09-29 12:46:22 +0200

    moved code around

--- src/environments_wrappers/ros2/lunaryard_20m_ros2.py
+++ src/environments_wrappers/ros2/lunaryard_20m_ros2.py
@@ -15,7 +15,7 @@ from omni.isaac.core.utils.extensions import enable_extension
 from omni.isaac.core import World
 from pxr import Gf
 # Custom libs
-from lunaryard import LabController
+from src.environments.lunaryard import LabController
 from robot import RobotManager
 
 # Enables ROS2
