693663c7db307d6af7411b724d8e8b8a47fc9e51
AntoineRichard <antoine.richard@uni.lu>
2024-08-29 19:18:09 +0200

    added header

--- src/terrain_management/large_scale_terrain/collider_manager.py
+++ src/terrain_management/large_scale_terrain/collider_manager.py
@@ -1,3 +1,11 @@
+__author__ = "Antoine Richard"
+__copyright__ = "Copyright 2024, Space Robotics Lab, SnT, University of Luxembourg, SpaceR"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Antoine Richard"
+__email__ = "antoine.richard@uni.lu"
+__status__ = "development"
+
 from typing import Tuple
 import dataclasses
 import numpy as np
