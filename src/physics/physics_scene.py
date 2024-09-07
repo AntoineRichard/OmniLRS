c19f9ba37953a1bf9450446e6e0250d055178a26
AntoineRichard <antoine.richard@uni.lu>
2024-09-08 00:25:13 +0200

    added header

--- src/physics/physics_scene.py
+++ src/physics/physics_scene.py
@@ -1,3 +1,11 @@
+__author__ = "Antoine Richard"
+__copyright__ = "Copyright 2023-24, Space Robotics Lab, SnT, University of Luxembourg, SpaceR"
+__license__ = "BSD 3-Clause"
+__version__ = "2.0.0"
+__maintainer__ = "Antoine Richard"
+__email__ = "antoine.richard@uni.lu"
+__status__ = "development"
+
 from src.configurations.physics_confs import PhysicsSceneConf
 
 from omni.isaac.core.physics_context.physics_context import PhysicsContext
--- src/physics/terramechanics_parameters.py
+++ src/physics/terramechanics_parameters.py
@@ -1,16 +1,27 @@
+__author__ = "Junnosuke Kamohara"
+__copyright__ = "Copyright 2023-24, Space Robotics Lab, SnT, University of Luxembourg, SpaceR"
+__license__ = "BSD 3-Clause"
+__version__ = "2.0.0"
+__maintainer__ = "Antoine Richard"
+__email__ = "antoine.richard@uni.lu"
+__status__ = "development"
+
 from dataclasses import dataclass
 import math
 import numpy as np
+
 
 @dataclass
 class RobotParameter:
     """
     Robot mechanical dimension parameters."""
+
     mass: float = 20.0
     num_wheels: int = 4
     wheel_radius: float = 0.09
     wheel_base: float = 0.1
     wheel_lug_height: float = 0.02
+
 
 @dataclass
 class TerrainMechanicalParameter:
@@ -18,6 +29,7 @@ class TerrainMechanicalParameter:
     Terrain mechanical parameters.
     - bearing_parameter: (k_c, k_phi, n_0, n_1) bearing parameters.
     - shearing_parameter: (c, phi, K) shearing parameters."""
+
     k_c: float = 0.0
     k_phi: float = 100.0
     a_0: float = 0.4
