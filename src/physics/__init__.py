7984027b09bae7791aebff0d4da87d1e8957321b
AntoineRichard <antoine.richard@uni.lu>
2024-09-08 20:50:44 +0200

    bumped version

---
+++ src/physics/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Antoine Richard"
+__copyright__ = "Copyright 2023-24, Space Robotics Lab, SnT, University of Luxembourg, SpaceR"
+__license__ = "BSD 3-Clause"
+__version__ = "2.0.0"
+__maintainer__ = "Antoine Richard"
+__email__ = "antoine.richard@uni.lu"
+__status__ = "development"
