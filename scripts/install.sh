85f92709a9924abccf5bc2c387b6e4a9afdfd88e
AntoineRichard <antoine.richard@uni.lu>
2024-09-10 11:15:50 +0200

    added install script

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pulls WorldBuilder
+git submodule init
+git submodule update
+
+# Install GDAL
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+version=$(gdal-config --version)
+
+# Install Python packages for Isaac Sim
+~/.local/share/ov/pkg/isaac_sim-2023.1.1/python.sh -m pip install opencv-python omegaconf hydra-core skyfield gdal==$version zfpy, gdown, black, numba
+
+# Download the assets from Google Drive
+gdown 1LfdJ8cogFU8Eid2EL-0bu9E383lftC_W
+unzip assets_v6.zip
+rm assets.zip
+
+# Download the DEMs of the lunar southpole and format them.
+./scripts/get_dems.sh
+./scripts/extract_dems_override.sh
+# Get Ephemeris data
+./scripts/get_ephemeris_data.sh
