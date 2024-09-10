8cd14784506a5f6ca2f413f6a3ab0c385f051f00
Andrej Orsula <orsula.andrej@gmail.com>
2024-09-10 15:30:23 +0200

    Docker: Copy Isaac Sim to root dir instead of home
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- docker.spaceros/run.sh
+++ docker.spaceros/run.sh
@@ -3,13 +3,10 @@ xhost +
 
 VOLUMES=(
     "${PWD}:/workspace/omnilrs:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/cache/kit:/home/spaceros-user/isaac-sim/kit/cache:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/cache/ov:/home/spaceros-user/.cache/ov:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/cache/pip:/home/spaceros-user/.cache/pip:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/cache/glcache:/home/spaceros-user/.cache/nvidia/GLCache:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/cache/computecache:/home/spaceros-user/.nv/ComputeCache:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/logs:/home/spaceros-user/.nvidia-omniverse/logs:rw"
-    "${HOME}/docker/omnilrs/isaac-sim/data:/home/spaceros-user/.local/share/ov/data:rw"
+    "${HOME}/docker/cache/omnilrs/isaac-sim/computecache:/home/spaceros-user/.nv/ComputeCache:rw"
+    "${HOME}/docker/cache/omnilrs/isaac-sim/glcache:/home/spaceros-user/.cache/nvidia/GLCache:rw"
+    "${HOME}/docker/cache/omnilrs/isaac-sim/kit:/isaac-sim/kit/cache:rw"
+    "${HOME}/docker/cache/omnilrs/isaac-sim/ov:/home/spaceros-user/.cache/ov:rw"
 )
 # Create cache directories with appropriate permissions for the user inside the container
 for VOLUME in "${VOLUMES[@]}"; do
@@ -19,7 +16,7 @@ for VOLUME in "${VOLUMES[@]}"; do
         chmod 777 "${HOST_DIR}"
     fi
 done
-find "${DEMO_DIR}" -exec bash -c 'p=$(stat -c "%a" "$0"); op=${p:0:1}; chmod "${op}${op}${op}" "$0" 2>/dev/null' {} \; 2>/dev/null || :
+find "${PWD}" -exec bash -c 'p=$(stat -c "%a" "$0"); op=${p:0:1}; chmod "${op}${op}${op}" "$0" 2>/dev/null' {} \; 2>/dev/null || :
 
 docker run --name isaac-sim-omnilrs-container -it --gpus all -e "ACCEPT_EULA=Y" --rm --network=host --ipc=host \
 -v $HOME/.Xauthority:/root/.Xauthority \
