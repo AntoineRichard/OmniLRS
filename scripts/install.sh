24efdb3e3e6e5e941a805d4f30c9cddae6e23472
AntoineRichard <antoine.richard@uni.lu>
2024-09-10 11:24:41 +0200

    fixed rm not working

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,7 @@ version=$(gdal-config --version)
 # Download the assets from Google Drive
 gdown 1LfdJ8cogFU8Eid2EL-0bu9E383lftC_W
 unzip assets_v6.zip
-rm assets.zip
+rm assets_v6.zip
 
 # Download the DEMs of the lunar southpole and format them.
 ./scripts/get_dems.sh
