733006ec946f763362605f423c5999a2e9426475
AntoineRichard <antoine.richard@uni.lu>
2024-09-11 10:11:59 +0200

    fixed issue with docker

--- spaceros_demo.docker/build_docker.sh
+++ spaceros_demo.docker/build_docker.sh
@@ -6,7 +6,7 @@ PROJECT_DIR="$(dirname "${SCRIPT_DIR}")"
 TAG="space-ros-omnilrs"
 DOCKERFILE="${SCRIPT_DIR}/Dockerfile"
 
-DOCKER_BUILD_CMD=(docker build "${SCRIPT_DIR}" --tag ${TAG})
+DOCKER_BUILD_CMD=(docker build --no-cache "${SCRIPT_DIR}" --tag ${TAG})
 
 echo -e "\033[0;32m${DOCKER_BUILD_CMD[*]}\033[0m" | xargs
 
--- spaceros_demo.docker/ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Setup the Space ROS environment
-source "${SPACEROS_DIR}/install/setup.bash"
-source "/home/spaceros-user/spaceros/demo/install/setup.bash"
-export IKOS_SCAN_NOTIFIER_FILES="" # make ikos create .ikosbin files for compiled packages
-
-exec "$@"
